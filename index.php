faedcbc4e97624b43b009cefcda9af76ede66e33
Radish Spirit <radishspirit88@gmail.com>
2017-12-28 21:52:36 -0700

    He-HAW

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <body>
         <header>
             <h1>Hey! I just wanted to say: Welcome to my new site!</h1>
-            <p>Stick around.</p>
+            <p>Stick around. Ass hat! Yo!</p>
         </header>
     </body>
 </html>
