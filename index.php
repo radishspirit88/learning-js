9a577b90e5213a45aa8c620b4c4b52fb91993cc7
Radish Spirit <radishspirit88@gmail.com>
2018-01-03 09:13:47 -0700

    Text edits on index.php

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <body>
         <header>
             <h1>Hey! I just wanted to say: Welcome to my new site!</h1>
-            <p>Stick around. Ass hat! Something different.</p>
+            <p>Stick around man! Ass hat! Something “different.”</p>
+            <p>We like people.</p>
+            <p>Hehe</p>
         </header>
     </body>
 </html>
