9d642c10f0c29aab877b28ebb3aee2829d2372a3
Radish Spirit <radishspirit88@gmail.com>
2018-01-03 09:30:38 -0700

    Add exclamation point.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
             <h1>Hey! I just wanted to say: Welcome to my new site!</h1>
             <p>Stick around man! Ass hat! Something “different.”</p>
             <p>We like people.</p>
-            <p>Hehe</p>
+            <p>Hehe!</p>
         </header>
     </body>
 </html>
